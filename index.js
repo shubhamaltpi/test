8b37b3496509a2fc8f96d7ac6e8e53eb7c7a74b6
shubhamaltpi <shubhamaltpi@gmail.com>
2023-12-22 12:03:33 +0530

    sj

---
+++ index.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const app = express();
+const port = 3002;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define a simple route
+app.get('/', (req, res) => {
+  res.send('Hello, this is your Express server!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
